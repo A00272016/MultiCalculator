a25c6b5fea9ece0659f656ba8d4a45af72a70ede
A00272016 <A00272016@mycambrian.ca>
2024-06-04 21:14:54 -0400

    Function created to Add numbers.

---
+++ shared/src/commonMain/kotlin/com/example/multicalculator/Calculator.kt
@@ -0,0 +1,8 @@
+// Copy the package declaration from Greeting.kt and paste it here
+package com.example.multicalculator
+
+class Calculator {
+    fun add(left: Int, right: Int): Int {
+        return left + right
+    }
+}
