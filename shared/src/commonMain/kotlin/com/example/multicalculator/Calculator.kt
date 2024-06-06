136e998e464f9401374232be04b8fbe114cf7da1
A00272016 <A00272016@mycambrian.ca>
2024-06-05 21:11:03 -0400

    Add Some Comments To the Code

--- shared/src/commonMain/kotlin/com/example/multicalculator/Calculator.kt
+++ shared/src/commonMain/kotlin/com/example/multicalculator/Calculator.kt
@@ -3,15 +3,19 @@
 package com.example.multicalculator
 
 class Calculator {
+    //Function Addition of two numbers
     fun add(left: Int, right: Int): Int {
         return left + right
     }
+    //Function for Subraction of two numbers
     fun subtract(left: Int, right: Int): Int {
         return left - right
     }
+    //Function Multiplication of two numbers
     fun multiply(left: Int, right: Int): Int {
         return left * right
     }
+    //Function Division of two numbers
     fun divide(left: Int, right: Int): Int {
         return left / right
     }
