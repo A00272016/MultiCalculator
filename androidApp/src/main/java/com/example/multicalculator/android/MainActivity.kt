7d1b61e1e63df02bc22d4cc97c66c984f7308080
A00272016 <A00272016@mycambrian.ca>
2024-06-13 20:19:38 -0400

    Created Function CalcRow For Calculator

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.multicalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.multicalculator.Greeting
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +21,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    CalcView()  // Use the new CalcView function here
                 }
             }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
 
-@Preview
+
+// This is a row in the calculator that holds numeric buttons
 @Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
+    val endNum = startNum + numButtons
+    Row(modifier = Modifier.padding(0.dp)) {
+        for (i in startNum until endNum) {
+            CalcNumericButton(number = i, display = display)
+        }
     }
 }
+
